a6da11afbe5d03f9a345671f03610f561060530e
Kim Benedict Santiago <kim@Kims-MacBook-Pro.local>
2023-05-10 16:55:30 +0800

    Added Element UI and jsPDF dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/element-ui', as: 'script' },
+    { src: '~/plugins/jsPDF', as: 'script' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
