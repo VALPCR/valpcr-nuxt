b2c847632318338d4a6f856f71d39a2f3f330718
Kim Benedict Santiago <kim@Kims-MacBook-Pro.local>
2023-05-10 16:48:47 +0800

    Added Message and Toast components

---
+++ components/notification/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="message text-danger my-3">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorMessage',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+.message {
+  font-size: 1.1rem;
+}
+</style>
