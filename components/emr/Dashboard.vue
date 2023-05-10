dda21c7314c930d8fb9220004cf4002031826268
Kim Benedict Santiago <kim@Kims-MacBook-Pro.local>
2023-05-10 21:09:09 +0800

    added empty dashboard for emr

---
+++ components/emr/Dashboard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Code here</div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/dashboard/emr/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <Dashboard />
+</template>
+
+<script>
+import Dashboard from "../../../components/emr/Dashboard";
+export default {
+  components: { Dashboard }
+}
+</script>
