eebdb39c6827cb5fb906fcdd36a581c999f451f6
Kim Benedict Santiago <kim@Kims-MacBook-Pro.local>
2023-05-12 13:44:51 +0800

    Added static filters

--- components/emr/Dashboard.vue
+++ components/emr/Dashboard.vue
@@ -1,19 +1,103 @@
 <template>
   <div class="min-h-screen bg-[#D7E4F3]">
-    <vue-good-table
-      :columns="columns"
-      :rows="rows"
-      :search-options="{ enabled: true }"
-      :pagination-options="{ enabled: true }"
-      class="bg-white rounded shadow"
-    />
+    <div class="m-1">
+      <div class="p-4">
+        <Filter @filter="onFilter" />
+        <vue-good-table
+          :columns="filterColumns"
+          :rows="emptyRows"
+          :search-options="{ enabled: true }"
+        >
+          <div slot="table-actions-bottom">
+            <button
+              type="button"
+              class="inline-block rounded bg-white text-black px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:outline-none focus:ring-0">
+              CLEAR
+            </button>
+            <button
+              type="button"
+              class="inline-block rounded bg-[#1890FF] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+              SEARCH
+            </button>
+          </div>
+        </vue-good-table>
+      </div>
+    </div>
+    <div class="m-5">
+      <vue-good-table
+        :columns="columns"
+        :rows="rows"
+        :search-options="{ enabled: true }"
+        :pagination-options="{ enabled: true }"
+        compactMode
+        class="bg-white rounded shadow"
+      >
+        <div slot="table-actions">
+          <button
+            type="button"
+            class="inline-block rounded bg-[#30AD62] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-green-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+            ADD NEW
+          </button>
+        </div>
+      </vue-good-table>
+    </div>
   </div>
 </template>
 
 <script>
+import Filter from "../Filter";
 export default {
+  components: { Filter },
   data() {
     return {
+      filteredRows: [],
+      emptyRows: [],
+      filterColumns: [
+        {
+          label: 'Case',
+          field: 'case',
+          filterOptions: {
+            styleClass: 'class1', // class to be added to the parent th element
+            enabled: true, // enable filter for this column
+            filterDropdownItems: ['Brain Hemorrhage', 'Car Accident', 'Heart Attack', 'Heart Stroke', 'Heat Stroke', 'Motorcycle Accident'],
+            trigger: 'enter', //only trigger on enter not on keyup
+          },
+          sortable: false,
+        },
+        {
+          label: 'Date',
+          field: 'date',
+          filterOptions: {
+            styleClass: 'class1', // class to be added to the parent th element
+            enabled: true, // enable filter for this column
+            filterDropdownItems: ['Today', 'Yesterday', 'This Week', 'This Month', 'Past 3 Months', 'This Year'],
+            trigger: 'enter', //only trigger on enter not on keyup
+          },
+          sortable: false,
+        },
+        {
+          label: 'Location',
+          field: 'location',
+          filterOptions: {
+            styleClass: 'class1', // class to be added to the parent th element
+            enabled: true, // enable filter for this column
+            filterDropdownItems: ['Arkong Bato', 'Bagbaguin', 'Balangkas', 'Bignay', 'Bisig', 'Canumay East', 'Canumay West', 'Coloong', 'Dalandanan', 'Gen T. De Leon', 'Isla', 'Karuhatan', 'Lawang Bato', 'Lingunan', 'Mabolo', 'Malanday', 'Malinta', 'Mapulang Lupa', 'Marulas', 'Maysan', 'Palasan', 'Parada', 'Pariancillo Villa', 'Paso De Blas', 'Pasolo', 'Poblacion', 'Pulo', 'Punturin', 'Rincon', 'Tagalag', 'Ugong', 'Viente Reales', 'Wawang Pulo'],
+            trigger: 'enter', //only trigger on enter not on keyup
+          },
+          sortable: false,
+        },
+        {
+          label: 'Ambulance',
+          field: 'ambulance',
+          filterOptions: {
+            styleClass: 'class1', // class to be added to the parent th element
+            enabled: true, // enable filter for this column
+            filterDropdownItems: ['EMS 01', 'EMS 02', 'EMS 03', 'EMS 04', 'EMS 05', 'EMS 06', 'EMS 07', 'EMS 08', 'EMS 09', 'EMS 10', 'EMS 11', 'EMS 12', 'EMS 13', 'EMS 14', 'EMS 15', 'EMS 16', 'EMS 17', 'EMS 18', 'EMS 19', 'EMS 20', 'EMS 21', 'EMS 22', 'EMS 23', 'EMS 24', 'EMS 25'],
+            trigger: 'enter', //only trigger on enter not on keyup
+          },
+          sortable: false,
+        },
+      ],
       columns: [
         {
           label: 'NAME',
@@ -21,7 +105,8 @@ export default {
         },
         {
           label: 'STATUS',
-          field: 'status',
+          field: 'radio',
+          html: true,
         },
         {
           label: 'TEAM',
@@ -39,19 +124,56 @@ export default {
         {
           label: 'CONTRACT',
           field: 'contract',
+          sortable: false,
         },
         {
           label: 'ACTIONS',
           field: 'actions',
+          sortable: false,
         },
       ],
       rows: [
-        { id:1, name:"Ava, Kate", status: "1", team: 'Alpha', address: 'Arkong Bato, Valenzuela City', age: 28, contract: 'loremimpusm@gmail.com', actions: "select" },
-        { id:1, name:"Baker, Hannah", status: "1", team: 'Bravo', address: 'Lingunan, Valenzuela City', age: 36, contract: 'loremimpusm@gmail.com', actions: "select" },
-        { id:1, name:"Carter, James", status: "1", team: 'Alpha', address: 'Maysan, Valenzuela City', age: 36, contract: 'loremimpusm@gmail.com', actions: "select" },
-        { id:1, name:"Dickson, Joey", status: "1", team: 'Alpha', address: 'Malinta, Valenzuela City', age: 36, contract: 'loremimpusm@gmail.com', actions: "select" },
+        { id:1, name:"Ava, Kate", radio: "<input\n" +
+            "  class=\"mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]\"\n" +
+            "  type=\"checkbox\"\n" +
+            "  role=\"switch\"\n" +
+            "  id=\"flexSwitchChecked\"\n" +
+            "  checked />", team: 'Alpha', address: 'Arkong Bato, Valenzuela City', age: 28, contract: 'loremimpusm@gmail.com', actions: "select" },
+        { id:1, name:"Baker, Hannah", radio: "<input\n" +
+            "  class=\"mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]\"\n" +
+            "  type=\"checkbox\"\n" +
+            "  role=\"switch\"\n" +
+            "  id=\"flexSwitchChecked\"\n" +
+            "  checked />", team: 'Bravo', address: 'Lingunan, Valenzuela City', age: 36, contract: 'loremimpusm@gmail.com', actions: "select" },
+        { id:1, name:"Carter, James", radio: "<input\n" +
+            "  class=\"mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]\"\n" +
+            "  type=\"checkbox\"\n" +
+            "  role=\"switch\"\n" +
+            "  id=\"flexSwitchChecked\"\n" +
+            "  checked />", team: 'Alpha', address: 'Maysan, Valenzuela City', age: 36, contract: 'loremimpusm@gmail.com', actions: "select" },
+        { id:1, name:"Dickson, Joey", radio: "<input\n" +
+            "  class=\"mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]\"\n" +
+            "  type=\"checkbox\"\n" +
+            "  role=\"switch\"\n" +
+            "  id=\"flexSwitchChecked\"\n" +
+            "  checked />", team: 'Alpha', address: 'Malinta, Valenzuela City', age: 36, contract: 'loremimpusm@gmail.com', actions: "select" },
       ],
     };
+  },
+  created() {
+    this.filteredRows = this.rows;
+  },
+  methods: {
+    onFilter({ field, value }) {
+      this.filteredRows = this.rows.filter(row => {
+        if (field === 'date') {
+          // convert both dates to YYYY-MM-DD format before comparing
+          return row[field] && new Date(row[field]).toISOString().slice(0,10) === value;
+        } else {
+          return row[field] && row[field].toString().toLowerCase().includes(value.toString().toLowerCase());
+        }
+      });
+    },
   },
 }
 </script>
