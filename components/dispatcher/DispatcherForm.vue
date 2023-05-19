ba22b4dfd3e46f6f7f5c063a528e2ef7a33fa1e1
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-05-19 14:23:53 +0800

    Added zip code for DispatcherForm

--- components/dispatcher/DispatcherForm.vue
+++ components/dispatcher/DispatcherForm.vue
@@ -105,21 +105,6 @@
                   Last Name
                 </label>
               </div>
-
-              <div class="relative mb-1" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  class="peer block min-h-[auto] rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="userName"
-                  aria-describedby="userName"
-                  placeholder="Username" />
-                <label
-                  for="userName"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                >
-                  Username
-                </label>
-              </div>
               <!-- END NAME -->
               <!-- SEX -->
               <select data-te-select-init class="w-full bg-neutral-50">
@@ -236,7 +221,7 @@
 
           <!-- END CONTACT -->
           <!-- EMAIL -->
-          <div class="email col-span-2">
+          <div class="email">
             <div class="relative mb-1" data-te-input-wrapper-init>
                 <input
                   type="email"
@@ -252,6 +237,23 @@
                 </label>
               </div>
           </div>
+
+              <div class="emazipil">
+                <div class="relative mb-1" data-te-input-wrapper-init>
+                  <input
+                    type="text"
+                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    id="zip"
+                    aria-describedby="zip"
+                    placeholder="Zip code" />
+                  <label
+                    for="zip"
+                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                  >
+                    Zip code
+                  </label>
+                </div>
+              </div>
 
           <!-- END EMAIL -->
             </div>
