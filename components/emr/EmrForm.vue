19d92647d02c53962b34c953c45538106d069cbe
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-05-27 13:00:04 +0800

    Fix EMR form registration

--- components/emr/EmrForm.vue
+++ components/emr/EmrForm.vue
@@ -55,7 +55,6 @@
             <div
               class="grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4"
             >
-              <!-- NAME -->
               <div class="relative mb-1" data-te-input-wrapper-init>
                 <input
                   v-model="suffix"
@@ -123,8 +122,7 @@
                   Last Name
                 </label>
               </div>
-              <!-- END NAME -->
-              <!-- SEX -->
+
               <select
                 v-model="gender"
                 data-te-select-init
@@ -133,9 +131,7 @@
                 <option value="female">Female</option>
                 <option value="male">Male</option>
               </select>
-              <label data-te-select-label-ref>Sex</label>
-              <!-- END SEX -->
-              <!-- BIRTHDATE -->
+
               <div
                 class="relative mb-3"
                 data-te-datepicker-init
@@ -171,10 +167,9 @@
                   Age
                 </label>
               </div>
-              <!-- END BIRTHDATE -->
-              <!-- TEAM -->
+
               <select
-                v-model="team"
+                v-model="team_id"
                 data-te-select-init
                 class="w-full bg-neutral-50"
               >
@@ -186,13 +181,9 @@
                   {{ item.name }}
                 </option>
               </select>
-              <label data-te-select-label-ref>Team</label>
-              <!-- END TEAM -->
-              <!-- ADDRESS -->
 
               <select v-model="city" data-te-select-init class="bg-neutral-50">
-                <option value="----">Select City</option>
-                <option selected value="valenzuela">City of Valenzuela</option>
+                <option value="Valenzuela">City of Valenzuela</option>
               </select>
 
               <select
@@ -201,122 +192,112 @@
                 class="bg-neutral-50"
               >
                 <option selected value="1">Select Barangay</option>
-                <option value="2">Arkong Bato</option>
-                <option value="3">Bagbaguin</option>
-                <option value="4">Balangkas</option>
-                <option value="5">Bignay</option>
-                <option value="6">Canumay East (Canumay)</option>
-                <option value="7">Canumay West (Canumay)</option>
-                <option value="8">Coloong</option>
-                <option value="9">Dalandanan</option>
-                <option value="10">Gen T. De Leon</option>
-                <option value="11">Isla</option>
-                <option value="12">Karuhatan</option>
-                <option value="13">Lawang Bato</option>
-                <option value="14">Lingunan</option>
-                <option value="15">Mabolo</option>
-                <option value="16">Malanday</option>
-                <option value="17">Malinta</option>
-                <option value="18">Mapulang Lupa</option>
-                <option value="19">Marulas</option>
-                <option value="20">Maysan</option>
-                <option value="21">Palasan</option>
-                <option value="22">Parada</option>
-                <option value="23">Pariancillo Villa</option>
-                <option value="24">Paso De Blas</option>
-                <option value="25">Pasolo</option>
-                <option value="26">Poblacion</option>
-                <option value="27">Pulo</option>
-                <option value="28">Punturin</option>
-                <option value="29">Rincon</option>
-                <option value="30">Tagalag</option>
-                <option value="31">Ugong</option>
-                <option value="32">Viente Reales</option>
-                <option value="33">Wawang Pulo</option>
+                <option value="Arkong Bato">Arkong Bato</option>
+                <option value="Bagbaguin">Bagbaguin</option>
+                <option value="Balangkas">Balangkas</option>
+                <option value="Bignay">Bignay</option>
+                <option value="Canumay East">Canumay East</option>
+                <option value="Canumay West">Canumay West</option>
+                <option value="Coloong">Coloong</option>
+                <option value="Dalandanan">Dalandanan</option>
+                <option value="Gen T. De Leon">Gen T. De Leon</option>
+                <option value="Isla">Isla</option>
+                <option value="Karuhatan">Karuhatan</option>
+                <option value="Lawang Bato">Lawang Bato</option>
+                <option value="Lingunan">Lingunan</option>
+                <option value="Mabolo">Mabolo</option>
+                <option value="Malanday">Malanday</option>
+                <option value="Malinta">Malinta</option>
+                <option value="Mapulang Lupa">Mapulang Lupa</option>
+                <option value="Marulas">Marulas</option>
+                <option value="Maysan">Maysan</option>
+                <option value="Palasan">Palasan</option>
+                <option value="Parada">Parada</option>
+                <option value="Pariancillo Villa">Pariancillo Villa</option>
+                <option value="Paso De Blas">Paso De Blas</option>
+                <option value="Pasolo">Pasolo</option>
+                <option value="Poblacion">Poblacion</option>
+                <option value="Pulo">Pulo</option>
+                <option value="Punturin">Punturin</option>
+                <option value="Rincon">Rincon</option>
+                <option value="Tagalag">Tagalag</option>
+                <option value="Ugong">Ugong</option>
+                <option value="Viente Reales">Viente Reales</option>
+                <option value="Wawang Pulo">Wawang Pulo</option>
               </select>
-              <div class="col-span-2">
-                <div class="relative mb-1" data-te-input-wrapper-init>
-                  <input
-                    v-model="street"
-                    type="text"
-                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="streetAddress"
-                    aria-describedby="streetAddress"
-                    placeholder="Street Address"
-                  />
-                  <label
-                    for="streetAddress"
-                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  >
-                    Street Address
-                  </label>
-                </div>
+
+              <div class="relative mb-1 col-span-2" data-te-input-wrapper-init>
+                <input
+                  v-model="street"
+                  type="text"
+                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  id="streetAddress"
+                  aria-describedby="streetAddress"
+                  placeholder="Street Address"
+                />
+                <label
+                  for="streetAddress"
+                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >
+                  Street Address
+                </label>
               </div>
 
-              <!-- END ADDRESS -->
-              <!-- CONTACT -->
-              <div class="contact align-center">
-                <div class="relative mb-1 bg-neutral-50">
-                  <span
-                    class="absolute left-3 top-1/2 transform -translate-y-1/2 text-neutral-500"
-                  >
-                    +63
-                  </span>
-                  <input
-                    v-model="phone"
-                    type="tel"
-                    id="contactNumber"
-                    name="contactNumber"
-                    class="peer block min-h-[auto] w-full pl-10 rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    pattern="[0-9]*"
-                    inputmode="numeric"
-                    maxlength="10"
-                    required
-                  />
-                </div>
+              <div class="relative mb-1" data-te-input-wrapper-init>
+                <input
+                  v-model="zip"
+                  type="text"
+                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  id="zip"
+                  aria-describedby="zip"
+                  placeholder="Zip code"
+                />
+                <label
+                  for="zip"
+                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >
+                  Zip code
+                </label>
               </div>
 
-              <!-- END CONTACT -->
-              <!-- EMAIL -->
-              <div class="email">
-                <div class="relative mb-1" data-te-input-wrapper-init>
-                  <input
-                    v-model="email"
-                    type="email"
-                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="email"
-                    aria-describedby="email"
-                    placeholder="Email"
-                  />
-                  <label
-                    for="email"
-                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  >
-                    Email
-                  </label>
-                </div>
+              <div
+                class="relative mb-1 bg-neutral-50"
+                data-te-input-wrapper-init
+              >
+                <span
+                  class="absolute left-3 top-1/2 transform -translate-y-1/2 text-neutral-500"
+                >
+                  +63
+                </span>
+                <input
+                  v-model="phone"
+                  type="tel"
+                  id="contactNumber"
+                  name="contactNumber"
+                  class="peer block min-h-[auto] w-full pl-10 rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  pattern="[0-9]*"
+                  inputmode="numeric"
+                  maxlength="10"
+                  required
+                />
               </div>
 
-              <div class="emazipil">
-                <div class="relative mb-1" data-te-input-wrapper-init>
-                  <input
-                    v-model="zip"
-                    type="text"
-                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="zip"
-                    aria-describedby="zip"
-                    placeholder="Zip code"
-                  />
-                  <label
-                    for="zip"
-                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  >
-                    Zip code
-                  </label>
-                </div>
+              <div class="relative mb-1 col-span-2" data-te-input-wrapper-init>
+                <input
+                  v-model="email"
+                  type="email"
+                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  id="email"
+                  aria-describedby="email"
+                  placeholder="Email"
+                />
+                <label
+                  for="email"
+                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >
+                  Email
+                </label>
               </div>
-
-              <!-- END EMAIL -->
             </div>
           </form>
         </div>
@@ -325,7 +306,6 @@
           class="mt-auto flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 min-[0px]:rounded-none"
         >
           <button
-            data-te-modal-dismiss
             @click="register"
             type="button"
             class="inline-block ml-4 rounded bg-[#30AD62] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-green-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
@@ -353,8 +333,8 @@ export default {
       phone: "",
       birthdate: "",
       email: "",
-      team: "alpha",
-      city: "",
+      team_id: "1",
+      city: "Valenzuela",
       barangay: "",
       street: "",
       zip: "",
@@ -365,7 +345,7 @@ export default {
   fetch() {
     this.$axios.get("team/list").then((response) => {
       response.data.return.map((team) =>
-        this.teams.push({ id: team.id, name: team.name })
+        this.teams.push({ id: team.id, name: this.capitalize(team.name) })
       );
     });
   },
@@ -375,7 +355,7 @@ export default {
   methods: {
     register() {
       const params = {
-        role: this.role,
+        team_id: parseInt(this.team_id),
         suffix: this.suffix,
         first_name: this.first_name,
         middle_name: this.middle_name,
@@ -386,32 +366,35 @@ export default {
           this.birthdate !== ""
             ? new Date(this.birthdate).toLocaleDateString("en-US")
             : (this.birthdate = ""),
+        age: this.age,
+        role: this.role,
         email: this.email,
-        team: parseInt(this.team),
         city: this.city,
         barangay: this.barangay,
         street: this.street,
         zip: this.zip,
-        age: this.age,
       };
 
       this.$axios.post("user/register", params).then(() => {
-        this.role = "emr";
         this.suffix = "";
         this.first_name = "";
         this.middle_name = "";
         this.last_name = "";
-        this.gender = "";
+        this.gender = "female";
         this.phone = "";
         this.birthdate = "";
         this.email = "";
-        this.team = "";
-        this.city = "";
+        this.role = "emr";
+        this.team_id = "1";
+        this.city = "Valenzuela";
         this.barangay = "";
         this.street = "";
         this.zip = "";
         this.age = "";
       });
+    },
+    capitalize(word) {
+      return word.replace(/^\w/, (c) => c.toUpperCase());
     },
   },
 };
