59d903fab04c6c8a29b4fcb1fa591749d7729be0
IM-sga <65697783+IM-sga@users.noreply.github.com>
2023-05-29 04:16:48 +0800

    Added V-model to Assigned EMR search bar

--- components/patient/PatientForm.vue
+++ components/patient/PatientForm.vue
@@ -681,9 +681,12 @@
                   </div>
                 </div>
               </div>
+              <div />
+              <div />
 
               <div class="relative mb-3" data-te-input-wrapper-init>
                 <input
+                  v-model="assignedEmr"
                   type="search"
                   class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   id="exampleSearch2"
@@ -769,6 +772,7 @@ export default {
       barangay_b: "",
       priority: "",
       remarks: "",
+      assignedEmr: "",
     };
   },
 
@@ -821,6 +825,7 @@ export default {
       this.barangay_b = "";
       this.priority = "";
       this.remarks = "";
+      this.assignedEmr = "";
     },
 
     register() {
@@ -859,6 +864,7 @@ export default {
         barangay_b: this.barangay_b,
         // priority: this.priority,
         remarks: this.remarks,
+        assignedEmr: this.assignedEmr,
       };
 
       this.$axios.post("pcr/register", params).then(() => {
@@ -896,6 +902,7 @@ export default {
         this.barangay_b = "";
         this.priority = "";
         this.remarks = "";
+        this.assignedEmr = "";
       });
     },
   },
