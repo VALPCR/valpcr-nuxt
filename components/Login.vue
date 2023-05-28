ef0c95acc5977e85a6fee2ba6c35b7a8d904da41
IM-sga <65697783+IM-sga@users.noreply.github.com>
2023-05-28 21:17:34 +0800

    Changed Sign In to Log In

--- components/Login.vue
+++ components/Login.vue
@@ -93,7 +93,7 @@
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
             type="submit"
           >
-            Sign In
+            Log In
           </button>
         </form>
       </div>
