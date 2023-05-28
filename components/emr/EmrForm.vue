fa63c7f4c5a6e24e15c718e63ebbc1b053820ad2
IM-sga <65697783+IM-sga@users.noreply.github.com>
2023-05-28 20:23:41 +0800

    Changed contact number maxlength to 11

--- components/emr/EmrForm.vue
+++ components/emr/EmrForm.vue
@@ -289,7 +289,7 @@
                   class="peer block min-h-[auto] w-full pl-10 rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   pattern="[0-9]*"
                   inputmode="numeric"
-                  maxlength="10"
+                  maxlength="11"
                   required
                 />
               </div>
@@ -352,7 +352,7 @@
                   class="peer block min-h-[auto] w-full pl-10 rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   pattern="[0-9]*"
                   inputmode="numeric"
-                  maxlength="10"
+                  maxlength="11"
                   required
                 />
               </div>
