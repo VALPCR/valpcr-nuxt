19e6c608d6e49fa019f67a22fd71963408c96686
IM-sga <65697783+IM-sga@users.noreply.github.com>
2023-05-30 04:37:03 +0800

    Changed Tab Name

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "valpcr-nuxt",
+    title: "ValPCR",
     htmlAttrs: {
       lang: "en",
     },
