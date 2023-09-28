71b178e1bdc2b2ae032a84e9de70eec626093891
John Michael Pores <capstone.johnmichaelpores@gmail.com>
2023-09-28 21:40:35 +0800

    Modified Forgot Layout

--- components/Forgot.vue
+++ components/Forgot.vue
@@ -1,32 +1,25 @@
 <template>
-  <div class="min-h-screen flex flex-col lg:flex-row">
-    <div
-      class="lg:w-1/2 bg-cover bg-center flex items-center justify-center text-white"
-      style="background-image: url('/images/valpcr_login_bg.jpg')"
-    >
-      <div class="text-center px-6">
-        <img
+  <section class="bg-gray-50">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <img
           src="/images/valpcr_logo.svg"
           height="auto"
-          width="50%"
+          width="7%"
+          mr="2"
           class="mx-auto mb-4"
           alt="valpcr logo"
         />
-        <h2 class="text-4xl font-bold mb-4">Welcome to ValPCR</h2>
-        <p>The first electronic patient care reporting (EPCR)</p>
-        <p>system in Valenzuela City.</p>
-      </div>
-    </div>
-    <div
-      class="lg:w-1/2 bg-[#D7E4F3] text-white flex items-center justify-center"
-    >
+        <h1 class="text-7xl font-bold text-dark-1 mx-auto mb-8"><i> Welcome to ValPCR! </i></h1>
       <div class="p-8 max-w-md w-full bg-white rounded-lg shadow-lg">
-        <h1 class="text-3xl mb-8 text-center text-black">FORGOT PASSWORD</h1>
-        <p class="ml-1 mr-1 text-center text-black mb-8">
-          Please verify with your email
+        <h1 class="text-3xl mb-2 text-center text-black font-bold">Forgot Password?</h1>
+        <p class="ml-1 mr-1 text-center text-gray-500 mb-4">
+          Please enter the email you use to log in to ValPCR
+        </p>
+        <p class="ml-1 mr-1 text-left text-gray-500">
+          Your email
         </p>
         <form @submit.prevent="forgot">
-          <div class="relative flex flex-nowrap items-stretch mb-3">
+          <div class="relative flex flex-nowrap items-stretch mb-5">
             <span
               class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
             >
@@ -35,14 +28,14 @@
             <input
               v-model="email"
               type="text"
-              class="relative m-0 block w-[1px] min-w-0 flex-auto rounded-r border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+              class="relative m-0 block w-[1px] min-w-0 flex-auto rounded-r border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary "
               placeholder="Email"
               aria-label="Email"
               aria-describedby="addon-wrapping"
             />
           </div>
           <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-5"
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-5"
             type="submit"
           >
             Reset Password
@@ -57,7 +50,7 @@
         </form>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
