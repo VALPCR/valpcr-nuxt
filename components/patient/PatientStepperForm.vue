c30e4d35d0b0f2dc6be4a0d5af04755a42801860
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-10-22 10:11:03 +0800

    Removed unnecessary function

--- components/patient/PatientStepperForm.vue
+++ components/patient/PatientStepperForm.vue
@@ -3069,7 +3069,6 @@
                       ref="received_by_signature"
                       :w="'600px'"
                       :h="'200px'"
-                      @input="receivedBySignature()"
                     />
                     <label
                       class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -3976,10 +3975,6 @@ export default {
         this.posterior_right_leg_injury = "";
         location.reload();
       });
-    },
-    receivedBySignature() {
-      this.received_by_signature = this.$refs.received_by_signature.save()
-      console.log(this.received_by_signature);
     },
   },
 };
