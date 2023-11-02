abb051fbe57aaa955d956e0f7f67eb3913697f02
JM Pores <capstone.johnmichaelpores@gmail.com>
2023-11-03 07:17:20 +0800

    Add modals

--- components/modals/ActionDelete.vue
+++ components/modals/ActionDelete.vue
@@ -2,7 +2,6 @@
   <div class="modal-mask">
     <div class="modal-wrapper">
       <div class="modal-container">
-        <h3 class="text-primary font-bold">{{ title }}</h3>
         <div>
           <slot></slot>
         </div>
@@ -10,30 +9,32 @@
           <!-- TAKE NOTE!! yung $emit will trigger an action on parent with the @emit-name directive -->
           <!-- tulad dito, @emit('cancel-action') so pag pinindot sya, ititrigger nya yung function na nasa @cancel-action sa parent nya -->
           <!-- Pwede ka maglagay ng hanggat ilang actions mo gusto pero need mo syang ipartner sa emit action -->
-
           <button
-            class="inline-block rounded bg-info px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
-            @click="$emit('cancel-action')" data-te-ripple-init data-te-ripple-color="light">CANCEL</button>
+            class="inline-block rounded bg-blue-500 hover:bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
+            @click="$emit('confirm-action')" data-te-ripple-init data-te-ripple-color="light">
+            Yes
+          </button>
           <button
-            class="inline-block rounded bg-red-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
-            @click="$emit('delete-action')" data-te-ripple-init data-te-ripple-color="light">DELETE</button>
+            class="inline-block rounded bg-blue-500 hover:bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
+            @click="$emit('cancel-action')" data-te-ripple-init data-te-ripple-color="light">
+            No
+          </button>
         </footer>
       </div>
     </div>
   </div>
 </template>
-  
+
 <script>
 export default {
   props: {
     title: {
       type: String,
-      required: true
-    }
-  }
-}
+    },
+  },
+};
 </script>
-  
+
 <style>
 .modal-mask {
   position: fixed;
@@ -42,9 +43,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, .5);
+  background-color: rgba(0, 0, 0, 0.5);
   display: table;
-  transition: opacity .3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .modal-wrapper {
@@ -57,11 +58,11 @@ export default {
   width: 500px;
   margin: 0px auto;
   padding: 20px 30px;
-  text-align: left;
+  text-align: center;
   background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
 }
 
 footer {
--- components/modals/StatusChange.vue
+++ components/modals/StatusChange.vue
@@ -2,7 +2,6 @@
   <div class="modal-mask">
     <div class="modal-wrapper">
       <div class="modal-container">
-        <h3 class="text-primary font-bold">{{ title }}</h3>
         <div>
           <slot></slot>
         </div>
@@ -12,28 +11,26 @@
           <!-- Pwede ka maglagay ng hanggat ilang actions mo gusto pero need mo syang ipartner sa emit action -->
 
           <button
-            class="inline-block rounded bg-info px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
-            @click="$emit('cancel-action')" data-te-ripple-init data-te-ripple-color="light">CANCEL</button>
-          <button
-            class="inline-block rounded bg-red-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
-            @click="$emit('delete-action')" data-te-ripple-init data-te-ripple-color="light">DELETE</button>
+            class="inline-block rounded bg-blue-500 hover:bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
+            @click="$emit('confirm-action')" data-te-ripple-init data-te-ripple-color="light">
+            OK
+          </button>
         </footer>
       </div>
     </div>
   </div>
 </template>
-  
+
 <script>
 export default {
   props: {
     title: {
       type: String,
-      required: true
-    }
-  }
-}
+    },
+  },
+};
 </script>
-  
+
 <style>
 .modal-mask {
   position: fixed;
@@ -42,9 +39,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, .5);
+  background-color: rgba(0, 0, 0, 0.5);
   display: table;
-  transition: opacity .3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .modal-wrapper {
@@ -57,11 +54,11 @@ export default {
   width: 500px;
   margin: 0px auto;
   padding: 20px 30px;
-  text-align: left;
+  text-align: center;
   background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
 }
 
 footer {
