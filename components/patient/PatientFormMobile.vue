8d58f759f3f5ce7c03a680e6fd6253b912e6a987
IM-sga <hermesaniesteph@gmail.com>
2023-11-09 22:46:49 +0800

    Changed score field to readonly

--- components/patient/PatientFormMobile.vue
+++ components/patient/PatientFormMobile.vue
@@ -1842,6 +1842,7 @@
                           v-model="score"
                           type="text"
                           class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.33rem] text-xs leading-[1.5] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          readonly
                         />
                       </div>
                     </div>                    
