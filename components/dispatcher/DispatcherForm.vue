ca90061b344ceb2987a84d4f8c5419e25107c829
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-11-17 15:04:12 +0800

    Bug fix: Removed zip

--- components/dispatcher/DispatcherForm.vue
+++ components/dispatcher/DispatcherForm.vue
@@ -273,24 +273,6 @@
                 </label>
               </div>
 
-              <div class="relative mb-1" data-te-input-wrapper-init>
-                <input
-                  v-model="zip"
-                  type="text"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="zip"
-                  aria-describedby="zip"
-                  placeholder="Zip code"
-                />
-                <label
-                  for="zip"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                >
-                  Zip code
-                  <span class="text-red-600 font-bold"> * </span>
-                </label>
-              </div>
-
               <div
                 class="relative mb-1 bg-neutral-50"
                 data-te-input-wrapper-init
@@ -379,7 +361,6 @@ export default {
       city: "Valenzuela",
       barangay: "",
       street: "",
-      zip: "",
       age: "",
       teams: [],
     };
@@ -414,7 +395,6 @@ export default {
         city: this.city,
         barangay: this.barangay,
         street: this.street,
-        zip: this.zip,
       };
 
       this.$nuxt.$loading.start();
@@ -435,7 +415,6 @@ export default {
           this.city = "Valenzuela";
           this.barangay = "";
           this.street = "";
-          this.zip = "";
           this.$emit("refresh");
           location.reload();
         })
@@ -461,7 +440,6 @@ export default {
       this.city = "Valenzuela";
       this.barangay = "";
       this.street = "";
-      this.zip = "";
     },
   },
 };
