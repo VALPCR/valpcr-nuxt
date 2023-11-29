f251a9f73156838c34d1b2bbc5827239599491e3
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-11-29 14:25:36 +0800

    Converted timestamp under Audit trail to human readable format

--- components/audit/Dashboard.vue
+++ components/audit/Dashboard.vue
@@ -86,10 +86,24 @@ export default {
     this.role = this.$auth.user.role;
     this.$axios.get("pcr/audit").then((response) => {
       response.data.return.map((result) => {
+
+        const dateCreated = new Date(result.created_at);
+        const dateModified = new Date(result.updated_at);
+
+        const options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+          timeZoneName: "short",
+        };
+
         this.fetchedRows.push({
           id: result.id,
-          date_created: result.created_at,
-          date_modified: result.updated_at,
+          date_created: dateCreated.toLocaleString("en-US", options),
+          date_modified: dateModified.toLocaleString("en-US", options),
           event: result.event,
           user_name: result.user_name,
           user_role: result.user_role === 'emr' ? 'Responder' : (result.user_role === 'head' ? 'Head' : 'Dispatcher'),
