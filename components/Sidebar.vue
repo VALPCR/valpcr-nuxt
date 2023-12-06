ae7afa98075db9cccef70f294fda262634e86bfb
IM-sga <hermesaniesteph@gmail.com>
2023-12-06 16:47:16 +0800

    Modified Sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -48,7 +48,7 @@
         </svg>
       </button>
     </div>
-    <ul class="">
+    <ul class="space-y-2 font-medium">
       <!-- HEAD -->
       <nuxt-link
         v-if="role === 'head'"
@@ -63,7 +63,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !$store.getters['getSideBarState'],
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon :icon="['fas', 'file-medical']" size="lg" />
           <!-- <svg class="svg-icon text-white fill-current  hover:fill-[#1B295A]" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20" ><path d="M680 729.333q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35ZM440 1009.33V893.333q0-21 10-39.5t28-29.5q29.333-17.666 61.167-30.166 31.833-12.5 65.499-19.834L680 870l75.334-95.667q33.666 7.334 64.999 19.834 31.334 12.5 60.667 30.166 18 11 28.5 29.5t10.5 39.5v115.997H440Zm-66.666-115.997V936H186.666q-27.5 0-47.083-19.583T120 869.334V282.666q0-27.5 19.583-47.083T186.666 216h586.668q27.5 0 47.083 19.583T840 282.666v231.335Q814.333 471 772.667 446.834 731 422.667 680 422.667V376H280v66.666h320.668q-32 14.667-57.001 40.667-25 26-38 59.334H280v66.666h213.334q0 27.667 8.167 53 8.166 25.334 22.833 47.001H280V776h150.667q-27 21-42.167 52.167-15.166 31.166-15.166 65.166Z"/></svg> -->
@@ -92,13 +92,12 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !$store.getters['getSideBarState'],
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <!-- <font-awesome-icon icon="fa-solid fa-shield" /> -->
           <font-awesome-icon :icon="['fass', 'headset']" size="lg" />
           <span
             v-if="$store.getters['getSideBarState']"
-            class="sm:text-xs md:text-sm lg:text-md xl:text-lg"
           >
             Dispatchers
             <span
@@ -123,7 +122,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !isSidebarOpen,
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <!-- <font-awesome-icon icon="fa-solid fa-bell-concierge" /> -->
           <font-awesome-icon icon="fa-solid fa-truck-medical" size="lg" />
@@ -152,7 +151,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !isSidebarOpen,
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon :icon="['fas', 'chart-simple']" size="lg" />
           <span v-if="isSidebarOpen">Analytics</span>
@@ -170,7 +169,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !isSidebarOpen,
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon
             :icon="['fas', 'prescription-bottle-medical']"
@@ -191,7 +190,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !isSidebarOpen,
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon
             :icon="['fas', 'book']"
@@ -214,7 +213,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !isSidebarOpen,
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon :icon="['fas', 'file-medical']" size="lg" />
           <!-- <svg class="svg-icon text-white fill-current  hover:fill-[#1B295A]" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20" ><path d="M680 729.333q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35ZM440 1009.33V893.333q0-21 10-39.5t28-29.5q29.333-17.666 61.167-30.166 31.833-12.5 65.499-19.834L680 870l75.334-95.667q33.666 7.334 64.999 19.834 31.334 12.5 60.667 30.166 18 11 28.5 29.5t10.5 39.5v115.997H440Zm-66.666-115.997V936H186.666q-27.5 0-47.083-19.583T120 869.334V282.666q0-27.5 19.583-47.083T186.666 216h586.668q27.5 0 47.083 19.583T840 282.666v231.335Q814.333 471 772.667 446.834 731 422.667 680 422.667V376H280v66.666h320.668q-32 14.667-57.001 40.667-25 26-38 59.334H280v66.666h213.334q0 27.667 8.167 53 8.166 25.334 22.833 47.001H280V776h150.667q-27 21-42.167 52.167-15.166 31.166-15.166 65.166Z"/></svg> -->
@@ -242,7 +241,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !isSidebarOpen,
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <!-- <font-awesome-icon icon="fa-solid fa-bell-concierge" /> -->
           <font-awesome-icon icon="fa-solid fa-truck-medical" size="lg" />
@@ -271,7 +270,7 @@
             'sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]':
               !isSidebarOpen,
           }"
-          class="space-x-2 mb-4"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon
             :icon="['fas', 'prescription-bottle-medical']"
@@ -294,6 +293,7 @@
               ? 'space-x-2 mb-4 hover:bg-white md:pl-10 lg:pl-10 xl:pl-10 text-center md:text-left lg:text-left'
               : 'space-x-2 mb-4 sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]'
           }`"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon :icon="['fas', 'file-medical']" size="lg" /><span
             v-if="isSidebarOpen"
@@ -321,6 +321,7 @@
               ? 'space-x-2 mb-4 hover:bg-white md:pl-10 lg:pl-10 xl:pl-10 text-center md:text-left lg:text-left'
               : 'space-x-2 mb-4 sm:right-1.5 md:right-0 lg:right-0 xl:right-0 relative hover:text-[#1B295A]'
           }`"
+          class="space-x-2 mb-4 p-2 rounded-lg"
         >
           <font-awesome-icon :icon="['fas', 'clock-rotate-left']" size="lg" />
           <span v-if="isSidebarOpen">
