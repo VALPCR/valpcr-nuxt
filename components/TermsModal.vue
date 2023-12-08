4db0d0566a4cb85b88de31773c6824da470af7d3
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-12-08 13:48:59 +0800

    TermsModal before log in screen

--- components/TermsModal.vue
+++ components/TermsModal.vue
@@ -20,25 +20,6 @@
             class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
             Terms and Condition
           </h5>
-          <!--Close button-->
-          <button
-            type="button"
-            class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-            data-te-modal-dismiss
-            aria-label="Close">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="h-6 w-6">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
         </div>
 
         <!--Modal body-->
@@ -102,14 +83,6 @@
         <div
           class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
           <button
-            type="button"
-            class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-            data-te-modal-dismiss
-            data-te-ripple-init
-            data-te-ripple-color="light">
-            Close
-          </button>
-          <button
             v-if="showButton"
             type="button"
             class="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
@@ -138,10 +111,10 @@ export default {
     },
     handleScroll() {
       if (this.$device.isDesktop) {
-        this.showButton = this.$refs.scrollEvent.scrollTop >= 1000
+        this.showButton = this.$refs.scrollEvent.scrollTop >= 900
       } else if (this.$device.isTablet) {
         console.log(this.$refs.scrollEvent.scrollTop);
-        this.showButton = this.$refs.scrollEvent.scrollTop >= 800
+        this.showButton = this.$refs.scrollEvent.scrollTop >= 700
       }
     },
   },
